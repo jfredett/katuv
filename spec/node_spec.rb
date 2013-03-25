f79be58a15c6855b96d705d69c0311e740f65ff9
Joe Fredette <jfredett@gmail.com>
2013-03-25 18:09:22 -0400

    continue to flesh out the specs

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -5,37 +5,72 @@ describe Katuv::Node do
     class Example
       include Katuv::Node
     end
+
+    class ExampleTerminal
+      include Katuv::Node
+      terminal!
+    end
   end
   after { Object.send(:remove_const, :Example) }
 
-  context 'class methods' do
-    subject(:katuv_class) { Example }
 
-    describe 'api' do
-      it { should respond_to :nonterminal }
-      it { should respond_to :terminal    }
-      it { should respond_to :multiple    }
+  describe '#terminal!' do
+    context 'a terminal node' do
+      subject { ExampleTerminal }
+      it { should be_terminal }
     end
+
+    context 'making a node terminal' do
+      before { Example.should_not be_terminal }
+
+      it 'is made terminal by calling the #terminal! method' do
+        class Example
+          terminal!
+        end
+      end
+
+      after { Example.should be_terminal }
+    end
+
+
   end
 
-  context 'instance methods' do
-    subject(:katuv_class) { Example.new }
+  describe 'a simple, single-class example' do
+    context 'class methods' do
+      subject(:katuv_class) { Example }
 
-    describe 'api' do
-      it { should respond_to :each          }
-      it { should respond_to :children      }
-      it { should respond_to :has_children? }
-      it { should respond_to :terminal?     }
-      it { should respond_to :run           }
-      it { should respond_to :visit         }
+      describe 'api' do
+        it { should respond_to :nonterminal }
+        it { should respond_to :terminal    }
+        it { should respond_to :multiple    }
+      end
+
+      describe '#_type_to_method_name' do
+        it { should == Example }
+      end
+
+      describe
     end
 
-    its(:method_name) { should == :example }
+    context 'instance methods' do
+      subject(:katuv_class) { Example.new }
 
-    it { should_not have_children }
-    describe '#children' do
-      subject { katuv_class.children }
-      it { should be_empty }
+      describe 'api' do
+        it { should respond_to :each          }
+        it { should respond_to :children      }
+        it { should respond_to :has_children? }
+        it { should respond_to :terminal?     }
+        it { should respond_to :run           }
+        it { should respond_to :visit         }
+      end
+
+      its(:method_name) { should == :example }
+
+      it { should_not have_children }
+      describe '#children' do
+        subject { katuv_class.children }
+        it { should be_empty }
+      end
     end
   end
 end
