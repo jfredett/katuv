86d92bad8937ef04e46cf7419f1c53387924ef34
Joe Fredette <jfredett@gmail.com>
2013-04-09 08:50:00 -0400

    fix errant call to #run instead of #run!

--- lib/katuv/dsl.rb
+++ lib/katuv/dsl.rb
@@ -3,12 +3,12 @@ module Katuv
     def nonterminal(type)
       raise "NonterminalInTerminalError" if terminal?
       define_method(_type_to_method_name(type)) do |name=nil, opts={}, &block|
-      if children.has_key?(type) and children[type]
-        children[type].run(&block)
-      else
-        children[type] = type.new(name, opts.merge({parent: self}), &block)
-      end
-      children[type]
+        if children.has_key?(type) and children[type]
+          children[type].run!(&block)
+        else
+          children[type] = type.new(name, opts.merge(parent: self), &block)
+        end
+        children[type]
       end
     end
 
@@ -16,8 +16,8 @@ module Katuv
       #should only allow one.
       raise "InvalidNodeTypeError" unless type.terminal?
       define_method(_type_to_method_name(type)) do |name=nil, opts={}, &block|
-      raise "TerminalAlreadySetError" if children.has_key?(type)
-      children[type] = type.new(name, opts.merge({parent: self}), &block)
+        raise "TerminalAlreadySetError" if children.has_key?(type)
+        children[type] = type.new(name, opts.merge(parent: self), &block)
       end
     end
     def terminal!
@@ -30,8 +30,8 @@ module Katuv
       #it sees of this type. eg, `file 'x'; file 'y' #=> children[File] = [File<@name=x>, File<@name=y>]
       raise "InvalidNodeTypeError" unless type.terminal?
       define_method(_type_to_method_name(type)) do |name, opts={}, &block|
-      children[type] ||= ObjectSet.new
-      children[type] << type.new(name, opts.merge({parent: self}), &block)
+        children[type] ||= ObjectSet.new
+        children[type] << type.new(name, opts.merge(parent: self), &block)
       end
     end
 
