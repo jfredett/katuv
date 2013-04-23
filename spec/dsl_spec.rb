443bf55a1c463e70fa016c28751463394d13300c
Joe Fredette <jfredett@gmail.com>
2013-04-23 08:43:31 -0400

    add some more tests around the DSL

--- spec/dsl_spec.rb
+++ spec/dsl_spec.rb
@@ -5,18 +5,23 @@ describe 'The Katuv DSL' do
     class Example
     end
   end
-  subject { Example }
+  subject(:example) { Example }
 
   context 'before inclusion' do
     describe 'api' do
       it { should_not respond_to :terminal }
+      it { should_not respond_to :terminal! }
+      it { should_not respond_to :terminal? }
       it { should_not respond_to :nonterminal }
       it { should_not respond_to :multiple }
+
+      #technically private, but we need to test it... badly.
+      it { should_not respond_to :_type_to_method_name }
     end
   end
 
   context 'after inclusion' do
-    before do
+    before :all do
       class Example
         include Katuv::Node
       end
@@ -24,8 +29,23 @@ describe 'The Katuv DSL' do
 
     describe 'api' do
       it { should respond_to :terminal }
+      it { should respond_to :terminal! }
+      it { should respond_to :terminal? }
+
       it { should respond_to :nonterminal }
       it { should respond_to :multiple }
+
+      #technically private, but we need to test it... badly.
+      it { should respond_to :_type_to_method_name }
+    end
+
+    context do
+      describe '#_type_to_method_name' do
+        # this is kind of lousy
+        subject { example._type_to_method_name(example) }
+
+        it { should == 'example' }
+      end
     end
   end
 end
