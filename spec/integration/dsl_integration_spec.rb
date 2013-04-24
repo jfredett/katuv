2af6fff49d67ada30ea7ddc0182832841fdb9199
Joe Fredette <jfredett@gmail.com>
2013-04-24 07:24:56 -0400

    add integration spec for defining a katuv DSL

---
+++ spec/integration/dsl_integration_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+describe 'a dsl defined directly with katuv classes' do
+  before :all do
+    class Bar
+      include Katuv::Node
+      terminal!
+    end
+
+    class Baz
+      include Katuv::Node
+      terminal!
+
+      def self.name
+        'Quux'
+      end
+    end
+
+    class Foo
+      include Katuv::Node
+      multiple Bar
+      terminal Baz
+      nonterminal Foo
+    end
+  end
+
+  # A necessary bit of boilerplate required to start a script. you could,
+  # in theory, just do "Foo.new" to start the script, but you'd have to
+  # explicitly pass the parent.
+  #
+  # In needs to be in the outer scope (ie, not in the before block) so tests can
+  # get at it.
+  def foo(name = nil, opts={}, &block)
+    Foo.new(name, opts.merge(parent: nil), &block)
+  end
+
+  after :all do
+    Object.send(:remove_const, :Bar)
+    Object.send(:remove_const, :Baz)
+    Object.send(:remove_const, :Foo)
+  end
+
+  let :example_dsl_script do
+    foo do
+      bar '1'
+      bar '2'
+      quux 'bingle'
+
+      foo do
+        bar '3'
+        bar '4'
+
+        quux 'bangle'
+      end
+    end
+  end
+
+  it "parses the example script cleanly, raising no errors" do
+    expect { example_dsl_script }.to_not raise_error
+  end
+
+end
