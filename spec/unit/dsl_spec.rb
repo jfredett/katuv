052407f2f455eaa8fcdc67c6945421cc71aa8939
Joe Fredette <jfredett@gmail.com>
2013-10-09 11:28:03 -0400

    kill remaining mutants around Katuv::DSL.parse!

--- spec/unit/dsl_spec.rb
+++ spec/unit/dsl_spec.rb
@@ -6,4 +6,21 @@ describe Katuv::DSL do
     it { should respond_to :parse!     }
     it { should respond_to :define     }
   end
+
+  describe '#parse!' do
+    let(:definition) { double('definition of a dsl') }
+
+    before do
+      definition.stub(:ast).and_return([])
+    end
+
+    subject(:definition_ast) { Katuv::DSL.parse!(definition) }
+
+    specify "calls out to the definition to get it's ast" do
+      definition_ast # parse the dsl definition
+      definition.should have_received(:ast)
+    end
+
+    it { should == s(:dsl) }
+  end
 end
