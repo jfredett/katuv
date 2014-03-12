484d7f2324ae20bdd56bb19164f92878fda24459
Joe Fredette <jfredett@gmail.com>
2014-03-12 01:47:33 -0400

    tweak example DSL design and rough in a example sexp AST for it

--- spec/acceptance/dsl/interpreter_spec.rb
+++ spec/acceptance/dsl/interpreter_spec.rb
@@ -21,7 +21,7 @@ describe Katuv do
       end
 
       root :my_dsl do
-        maybe_one :Baz
+        maybe_many :Baz
         maybe_one :Bingle
       end
     end
@@ -38,14 +38,66 @@ describe Katuv do
         bar 'bar1' # foo is not required in a bar, so the block isn't either
       end
 
+      baz "baz3" do
+        foo 'foo3' do
+          some_block
+        end
+      end
+
+      baz "baz4" do
+        foo 'foo4', key: :value
+      end
+
+      baz "baz5" do
+        foo 'foo5', key: :value do
+          some_other_block
+        end
+      end
+
       bingle do #names aren't required
-        foo 'foo3'
+        foo 'foo6'
       end
     end
   end
 
+
+  # see notes/AST_DESIGN.mkd for details on the format.
   let(:expected_ast) do
-    #TODO: How should the AST be structured?
+    s(:my_dsl,
+      s(:meta, { name: 'Some Script', type: :root, optional?: false, data: {} }),
+      s(:children,
+        s(:baz,
+          s(:meta, { name: 'baz1', type: :nonterminal, optional?: false, data: {} }),
+          s(:type, :nonterminal),
+          s(:children,
+            s(:foo,
+              s(:meta, { name: 'foo1', type: :terminal, optional?: false, data: {} })))),
+        s(:baz,
+          s(:meta, { name: 'baz2', type: :nonterminal, optional?: false, data: {} }),
+          s(:children,
+            s(:foo,
+              s(:meta, { name: 'foo2', type: :terminal, optional?: false, data: {} })),
+            s(:bar,
+              s(:meta, { name: 'bar1', type: :terminal, optional?: true, data: {} })))),
+        s(:baz,
+          s(:meta, {name: 'baz3', type: :nonterminal, optional?: false, data: {}}),
+          s(:children,
+            s(:foo,
+              s(:meta, { name: 'foo3', type: :terminal, optional?: false, data: { block: double('some block') }})))),
+         s(:baz,
+          s(:meta, { name: 'baz4', type: :nonterminal, optional?: false, data: {}}),
+          s(:children,
+            s(:foo,
+              s(:meta, { name: 'foo4', type: :terminal, optional?: false, data: { key: :value }})))),
+         s(:baz,
+          s(:meta, { name: 'baz5', type: :nonterminal, optional?: false, data: {}}),
+          s(:children,
+            s(:foo,
+              s(:meta, { name: 'foo5', type: :terminal, optional?: false, data: { key: :value, block: double('some other block') }})))),
+         s(:bingle,
+           s(:meta, { name: nil, type: :nonterminal, optional?: true, data: {} }),
+           s(:children,
+             s(:foo, { name: 'foo6', type: :terminal, optional?: false, data: {} })))))
   end
 
   before do
