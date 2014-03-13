40b71c4c65721c2821b7858912c91a8d0eb2401a
Joe Fredette <jfredett@gmail.com>
2014-03-13 01:07:20 -0400

    implement an association interpreter

--- lib/katuv/core/interpreter/association.rb
+++ lib/katuv/core/interpreter/association.rb
@@ -1,19 +1,26 @@
 module Katuv
   module Core
     module Interpreter
-      class AssociationInterpreter < AST::Processor
+      class Association < AST::Processor
         attr_reader :name, :type, :optional
+
+        alias on_association process_all
+
         def on_name(node)
+          @name = node.children.first
         end
 
         def on_type(node)
+          @type = node.children.first
         end
 
         def on_optional?(node)
+          @optional = node.children.first
         end
 
         private :optional
         def optional?; optional; end
+        def required?; !optional; end
       end
     end
   end
