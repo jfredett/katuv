45152863d63ffc0de5fced6234a42cc26d590314
Joe Fredette <jfredett@gmail.com>
2014-03-13 01:07:20 -0400

    implement an association interpreter

---
+++ spec/acceptance/core/interpreter/association_spec.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe Katuv::Core::Interpreter::Association do
+  subject(:interpreter) { Katuv::Core::Interpreter::Association.new }
+
+  let(:example_associations_ast) do
+    s(:association,
+      s(:name, :Baz),
+      s(:type, :single),
+      s(:optional?, true))
+  end
+
+  before { interpreter.process(example_associations_ast) }
+
+  its(:name) { should == :Baz }
+  its(:type) { should == :single }
+  its(:optional?) { should be_true }
+end
