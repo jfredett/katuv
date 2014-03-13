81de52b05738af784514f2ff339d7c98e9f037b5
Joe Fredette <jfredett@gmail.com>
2014-03-12 22:04:55 -0400

    implement #on_dsl handler

--- spec/unit/core/interpreter_spec.rb
+++ spec/unit/core/interpreter_spec.rb
@@ -2,4 +2,20 @@
 require 'spec_helper'
 
 describe Katuv::Core::Interpreter do
+  subject(:interpreter) { Katuv::Core::Interpreter.new }
+
+  describe '#on_dsl' do
+    let(:namespace) { double('namespace element') }
+    let(:definition_subtree) { double('definition subtree') }
+    let(:sexp) { s(:dsl, namespace, definition_subtree ) }
+
+    before do
+      interpreter.stub(:process).with(namespace)
+      interpreter.stub(:process).with(definition_subtree)
+      interpreter.on_dsl(sexp)
+    end
+
+    it { should have_received(:process).with(namespace) }
+    it { should have_received(:process).with(definition_subtree) }
+  end
 end
